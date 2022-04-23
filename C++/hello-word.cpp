ed1abc72c7754be9ce4a7c06e50051675dbef795
yanlun <liyanlun@bytedance.com>
2022-04-23 20:41:09 +0800

    feat: restart learning C++

---
+++ C++/hello-word.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  char grade = 'A';
+  string name = "Anne";
+  int age = 28;
+  // float 
+  double gpa = 4.5;
+  bool isMale = false;
+
+  cout << "hello world! " << name << endl;
+  cout << name << " is " << age << " years old." << endl;
+  return 0;
+}
