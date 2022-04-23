8b131ab93499e08b0bf3ec8a65518e10613578cb
yanlun <liyanlun@bytedance.com>
2022-04-23 20:41:37 +0800

    feat: some practice on leecode

---
+++ Leetcode/206.反转链表.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=206 lang=javascript
+ *
+ * [206] 反转链表
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+  if (!head || !head.next) return head;
+  let cur = head;
+  let pre = null;
+  while (cur) {
+    next = cur.next;
+    cur.next = pre;
+    pre = cur;
+    cur = cur.next;
+  }
+  return pre;
+};
+// @lc code=end
+
---
+++ Leetcode/53.最大子数组和.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=53 lang=javascript
+ *
+ * [53] 最大子数组和
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+
+};
+// @lc code=end
+
